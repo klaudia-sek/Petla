4629865f4fbf9f33f0796eb2173cba371d2a3420
klaudia-sek <klaudia.anna.sek@gmail.com>
2022-11-23 18:58:39 +0100

    Uzycie petli dla imion kotow

--- src/For.java
+++ src/For.java
@@ -1,7 +1,8 @@
 public class For {
     public static void main(String[] args) {
-        for (int i=0;i<10;i++){
-            System.out.println("Romuald jest super");
+        String[] cats=new String[] {"Burek", "Mruczek", "Iskierka", "Pussy"};
+        for (int i=0;i< cats.length;i++){
+            System.out.println(cats[i]);
         }
     }
 }
